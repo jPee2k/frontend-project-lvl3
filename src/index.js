f3db5c68e086d8705b4fe5bb8cdbf25f9ca159d0
Serhii Pylypenko <jpee.dev@gmail.com>
2021-08-14 21:45:57 +0300

    fix path to file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import 'bootstrap/dist/js/bootstrap.min';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/app.scss';
 
-console.log('ehu!');
+// console.log('ehu!');
